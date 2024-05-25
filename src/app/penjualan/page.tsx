25d592f87d8261c1b06f41f8d99b7713bd7608e8
Naomi <gabriella.naomi21@ui.ac.id>
2024-05-25 13:17:35 +0700

    implement penjualan page

---
+++ src/app/penjualan/page.tsx
@@ -0,0 +1,21 @@
+// pages/index.js
+
+import Head from 'next/head';
+import Form from '../components/penjualan/Form';
+import { Container, Typography } from '@mui/material';
+
+export default function penjualan() {
+  return (
+    <>
+        <Container maxWidth="sm">
+        <Head>
+            <title> Penjualan</title>
+        </Head>
+        <Typography variant="h4" component="h1" gutterBottom>
+            Form untuk mendaftarkan Game Baru
+        </Typography>
+        <Form />
+        </Container>
+    </>
+  );
+}
