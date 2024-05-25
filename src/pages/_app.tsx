14868f1c021d01e3ef2e74989bb2e75625e4b49f
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-25 23:15:21 +0700

    Update _app.tsx for global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,8 @@
 import '../styles/globals.css';
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <header className="header">
-        <h1>GameTime</h1>
-      </header>
-      <main className="main">
-        <Component {...pageProps} />
-      </main>
-      <footer className="footer">
-        <p>&copy; 2024 GameTime</p>
-      </footer>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
